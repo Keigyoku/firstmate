6cdb0b36b16172d207d03df2b52be0455d0e36a4
Keigyoku <martinlight123@gmail.com>
2026-07-16 13:06:18 -0500

    Revert "fix: detach watcher launches from arm tasks (#20)"
    This reverts commit 6be376f3dd8772fb5602297b3a114fe58cceb256.

--- bin/fm-watch-launcher-lib.sh
+++ bin/fm-watch-launcher-lib.sh
@@ -18,16 +18,9 @@ fm_watch_launch_session() {
   local pid_var=$1 output=$2 launcher
   shift 2
   launcher=$(fm_watch_session_launcher) || return 1
-  # A new session does not detach file descriptors.
-  # Close every arm-task pipe and ignore SIGPIPE before exec so arm, restart,
-  # and post-race attach launches survive the harness stopping their parent task.
   case "$launcher" in
-    setsid)
-      (trap '' PIPE; exec setsid "$@") </dev/null >"$output" 2>&1 &
-      ;;
-    perl)
-      (trap '' PIPE; exec perl -MPOSIX=setsid -e 'setsid() >= 0 or die "setsid: $!\n"; exec @ARGV or die "exec: $!\n"' "$@") </dev/null >"$output" 2>&1 &
-      ;;
+    setsid) setsid "$@" >"$output" & ;;
+    perl) perl -MPOSIX=setsid -e 'setsid() >= 0 or die "setsid: $!\n"; exec @ARGV or die "exec: $!\n"' "$@" >"$output" & ;;
     *) return 1 ;;
   esac
   printf -v "$pid_var" '%s' "$!"
